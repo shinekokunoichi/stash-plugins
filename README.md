9c881afbae107fe3e264c78f762824f7caf036a2
shinekokunoichi <shinekokunoichi@gmail.com>
2026-01-21 02:31:46 +0100

    StashDB-Scraper 1.0

--- README.md
+++ README.md
@@ -5,8 +5,9 @@ https://shinekokunoichi.github.io/stash-plugins/main/index.yml
 <img width="446" height="354" alt="image" src="https://github.com/user-attachments/assets/96151086-5ae3-410c-b9ec-8d234418693e" />
 
 ## Plugins
+Note: Change plugins settings after installing them otherwise will use the default settings!
 ### Shinobi-Api
-Version: 1.0
+Version: 1.1
 Description: Custom Stash API powers all my plugins. It's free to use for others under the CC-BY-NC-ND-4.0 copyright. The API is fully commented in ECMA Script. If I get multiple requests for the API, I'll create a page with instructions for the basic functions.
 Current Modules:
 - UI: Graphic manipulator
@@ -17,7 +18,7 @@ Current Modules:
 - Task: Custom task
 - Scraper: Integrated scraper
 ### JAV-Finder
-Version: 1.0
+Version: 1.1
 Description: Custom JAV scraper.
 - Metadata in english and japanese
 - Auto updater on scene updating
@@ -25,3 +26,10 @@ Description: Custom JAV scraper.
 - Auto creation missing metadata
 - Filter for auto creation
 - Fallback option to change metadata language temporarily for the current scraping scene
+### StashDB-Scraper
+Version: 1.0
+Description: Auto update scenes, performers, groups and tags using StashDB (based on name and title)
+- Auto update and task update
+- Auto creation missing data
+- Filter for auto creation
+- Filter for performers gender creation
---
+++ plugins/StashDB-Scraper/StashDB-Scraper.js
@@ -0,0 +1,169 @@
+(async function () {
+	let settings = sk.plugins.get('StashDB-Scraper');
+    const defaultSettings = {
+        update: 'all',
+        autoCreate: true,
+        create: 'all',
+        genderFilter: 'both'
+    };
+
+    function genderFilter(gender) {
+        let notFilter = true;
+        const genderList = gender.split(' ');
+        genderList.forEach((value) => {
+            if (value != gender.toLowerCase()) notFilter = false;
+        });
+        return notFilter;
+    };
+
+    async function findStudios(studio) {
+        let exist = await sk.stashDB.studioExist(studio);
+        if (!isNaN(exist)) return exist;
+        if (!settings.create.includes('studios')) return;
+        exist = await sk.stashDB.toStashStudio(studio);
+        exist = await sk.stash.findStudio(studio.name);
+        return exist.id;
+    };
+
+    async function findPerformers(performers) {
+        let ids = [];
+        let exist = await sk.stashDB.performersExist(performers);
+        ids = exist.find;
+        if (!settings.create.includes('performers')) return ids;
+        for (performer of exist.notFind) {
+            if (genderFilter(performer.gender)) return;
+            let newPerformer = await sk.stashDB.toStashPerformer(performer);
+            newPerformer = await sk.stash.createPerformer(newPerformer);
+            ids.push(newPerformer.id);
+        };
+        return ids;
+    };
+
+    async function findTags(tags) {
+        let ids = [];
+        let exist = await sk.stashDB.tagsExist(tags);
+        ids = exist.find;
+        if (!settings.create.includes('tags')) return ids;
+        for (tag of exist.notFind) {
+            let newTag = await sk.stashDB.toStashTag(tag);
+            newTag = await sk.stash.createPerformer(newTag);
+            ids.push(newTag.id);
+        };
+        return ids;
+    };
+
+    function alreadyScraped(data) {
+        let exist = false;
+        data.stash_ids.forEach((id) => {
+            if (id.endpoint === sk.stashDB.getGQL()) exist = true;
+        });
+        return exist
+    };
+
+    //HOOK
+    async function scrapeScene(data, all) {
+        const scene = await sk.stash.findScene(data.id);
+        if (alreadyScraped(scene)) return;
+        const stashDB = await sk.stashDB.scene(data.title);
+        if (!stashDB) return;
+        stashDB.studio = await findStudios(stashDB.studio);
+        stashDB.performers = await findPerformers(stashDB.performers);
+        stashDB.tags = await findTags(stashDB.tags);
+        await sk.stashDB.toStash(stashDB, scene.id);
+        if (!all) sk.notify(`Scraped ${stashDB.title}`);
+    };
+
+    async function scrapePerformer(data, all) {
+        const performer = await sk.stash.findPerformer(data.id);
+        if (alreadyScraped(performer)) return;
+        const stashDB = await sk.stashDB.performer(data.name);
+        if (!stashDB) return;
+        await sk.stashDB.toStash(stashDB, performer.id);
+        if (!all) sk.notify(`Scraped ${stashDB.name}`);
+    };
+
+    async function scrapeStudio(data, all) {
+        const studio = await sk.stash.findStudio(data.id);
+        if (alreadyScraped(studio)) return;
+        const stashDB = await sk.stashDB.studio(data.name);
+        if (!stashDB) return;
+        stashDB.parent = await createStudio(stashDB.parent);
+        await sk.stashDB.toStash(stashDB, scene.id);
+        if (!all) sk.notify(`Scraped ${stashDB.name}`);
+    };
+
+    async function scrapeTag(data, all) {
+        const tag = await sk.stash.findTag(data.id);
+        if (alreadyScraped(tag)) return;
+        const stashDB = await sk.stashDB.tag(data.name);
+        if (!stashDB) return;
+        await sk.stashDB.toStash(stashDB, tag.id);
+        if (!all) sk.notify(`Scraped ${tag.name}`);
+    };
+
+    //TASK
+    async function scrapeAll() {
+        await scrapeScenes();
+        await scrapePerformers();
+        await scrapeStudios();
+        await scrapeTags();
+    };
+
+    async function scrapeScenes() {
+        const scenes = await sk.stash.findScenes();
+        sk.notify(`Scraping ${scenes.length} scenes`);
+        for (scene of scenes) {
+            await scrapeScene(scene);
+        };
+        sk.notify(`Finished scraping scenes`);
+    };
+
+    async function scrapePerformers() {
+        const performers = await sk.stash.findPerformers();
+        sk.notify(`Scraping ${performers.length} performers`);
+        for (performer of performers) {
+            await scrapePerformer(performer);
+        };
+        sk.notify(`Finished scraping performers`);
+    };
+
+    async function scrapeStudios() {
+        const studios = await sk.stash.findStudios();
+        sk.notify(`Scraping ${studios.length} studios`);
+        for (studio of studios) {
+            await scrapeStudio(studio);
+        };
+        sk.notify(`Finished scraping studios`);
+    };
+
+    async function scrapeTags() {
+        const tags = await sk.stash.findTag();
+        sk.notify(`Scraping ${tags.length} tags`);
+        for (tag of tags) {
+            await scrapeTag(tag);
+        };
+        sk.notify(`Finished scraping tags`);
+    };
+
+    async function main() {
+        if (!settings) settings = defaultSettings;
+		if (sk.stashDB.);
+        settings.update === 'all' ? settings.update = 'scenes, performers, studios, tags' : null;
+        settings.create === 'all' ? settings.create = 'scenes, performers, studios, tags' : null;
+        settings.genderFilter === 'both' ? settings.genderFilter = 'male, female' : null;
+        await sk.useNotification('StashDB-Scraper');
+        //HOOK
+        if (settings.update.includes('scenes')) sk.hook.watch('scene update', scrapeScene);
+        if (settings.update.includes('performers')) sk.hook.watch('performer create', scrapePerformer);
+        if (settings.update.includes('studios')) sk.hook.watch('studio create', scrapeStudio);
+        if (settings.update.includes('tags')) sk.hook.watch('tag create', scrapeTag);
+        //TASK
+        sk.task.new('StashDB-Scraper', 'Scrape all', 'Scrape all scene', scrapeAll);
+        sk.task.new('StashDB-Scraper', 'Scrape scene', 'Scrape all scene', scrapeScenes);
+        sk.task.new('StashDB-Scraper', 'Scrape performer', 'Scrape all scene', scrapePerformers);
+        sk.task.new('StashDB-Scraper', 'Scrape studio', 'Scrape all scene', scrapeStudios);
+        sk.task.new('StashDB-Scraper', 'Scrape tag', 'Scrape all scene', scrapeTags);
+    };
+
+    main();
+})();
