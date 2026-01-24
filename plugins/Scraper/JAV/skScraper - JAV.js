b669ea0da3ecb8cfcb1cc8a00a0219bd36275145
shinekokunoichi <shinekokunoichi@gmail.com>
2026-01-24 02:54:04 +0100

    Bug fixes

--- plugins/Scraper/JAV/skScraper - JAV.js
+++ plugins/Scraper/JAV/skScraper - JAV.js
@@ -145,7 +145,8 @@
         if (!javCode) return;
         let javData = await searchJav(javCode);
         if (!javData && settings.fallback) javData = await searchJav(javCode, fallback())
-        if (!javData[0] || javData[0].getAttribute('x-show').includes('recommendItems')) {
+        const isRecommend = javData[0].getAttribute('x-show');
+        if (!javData[0] || !isRecommend || isRecommend.includes('recommendItems')) {
             sk.notify('skScraper - JAV', `${javCode} not found`);
             return;
         }
